ad64cc1230ff926228b8c1d545a278651aed7be5
Kingsley Darko <nyaabilai@gmail.com>
2023-02-21 18:48:05 +0000

    21/02/2023

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,41 +11,13 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  // toSchedule(){
+  //   document.getElementById('schedule')?.scrollIntoView({behavior:"smooth"})
+  // }
 
-  toHome(){
-    document.getElementById('home')?.scrollIntoView({behavior:"smooth"})
-  }
-
-  toGlc(){
-    document.getElementById('aboutglc')?.scrollIntoView({behavior:"smooth"})
-  }
-
-  toGeneralOverseer(){
-    document.getElementById('generaloverseer')?.scrollIntoView({behavior:"smooth"})
-  }
-
-  toGive(){
-    document.getElementById('give')?.scrollIntoView({behavior:"smooth"})
-  }
-
-  toGallery(){
-    document.getElementById('gallery')?.scrollIntoView({behavior:"smooth"})
-  }
-
-  toContactUs(){
-    document.getElementById('contact')?.scrollIntoView({behavior:"smooth"})
-  }
-
-  toChristForAll(){
-    document.getElementById('forall')?.scrollIntoView({behavior:"smooth"})
-  }
-
-  toMorningDevotion(){
-    document.getElementById('morningdevotion')?.scrollIntoView({behavior:"smooth"})
-  }
-
-  toSchedule(){
-    document.getElementById('schedule')?.scrollIntoView({behavior:"smooth"})
+  toSchedule(toggle : string){
+    document.getElementById(toggle)?.scrollIntoView({behavior:"smooth"})
   }
 
 }
