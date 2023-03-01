5531f4cea7e07dd09806806a8ddcfdd702c1aea4
Kingsley Darko <nyaabilai@gmail.com>
2023-03-01 10:45:21 +0000

    01032023

--- src/app/about/glc/glc.component.ts
+++ src/app/about/glc/glc.component.ts
@@ -10,6 +10,24 @@ export class GlcComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll',this.show);
+  }
+  
+  show() {
+    var reveals=document.querySelectorAll('.show');
+
+    for(var i=0 ; i< reveals.length; i++){
+      var windowheight= window.innerHeight;
+      var revealtop=reveals[i].getBoundingClientRect().top;
+      var revealpoint=150;
+
+      if(revealtop<windowheight-revealpoint){
+        reveals[i].classList.add('active');
+      }
+      else{
+        reveals[i].classList.remove('active');
+      }
+    }
   }
 
 }
