07900d14f2545c948bd2fa2b27071bb0bef6bff7
Kingsley Darko <nyaabilai@gmail.com>
2023-04-25 01:34:21 +0000

    finalized

--- src/app/about/our-general-overseer/our-general-overseer.component.ts
+++ src/app/about/our-general-overseer/our-general-overseer.component.ts
@@ -10,23 +10,8 @@ export class OurGeneralOverseerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    window.addEventListener('scroll',this.reveal);
+   
   }
 
-  reveal() {
-    var reveals=document.querySelectorAll('.reveal');
-
-    for(var i=0 ; i< reveals.length; i++){
-      var windowheight= window.innerHeight;
-      var revealtop=reveals[i].getBoundingClientRect().top;
-      var revealpoint=150;
-
-      if(revealtop<windowheight-revealpoint){
-        reveals[i].classList.add('active');
-      }
-      else{
-        reveals[i].classList.remove('active');
-      }
-    }
-  }
+  
 }
--- src/app/feed1000/feed1000.component.ts
+++ src/app/feed1000/feed1000.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Component, ElementRef, HostListener, Renderer2, ViewChild, OnInit } from '@angular/core';
-=======
-import { Component, OnInit } from '@angular/core';
->>>>>>> dfe13eb999eacf4df21f0d47caadae146b72c598
 
 @Component({
   selector: 'app-feed1000',
@@ -10,19 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed1000.component.css']
 })
 export class Feed1000Component implements OnInit {
-<<<<<<< HEAD
   @ViewChild('myDiv') myDiv!: ElementRef;
 
   constructor(private renderer: Renderer2) { }
-=======
-
-  constructor() { }
->>>>>>> dfe13eb999eacf4df21f0d47caadae146b72c598
 
   ngOnInit(): void {
   }
 
-<<<<<<< HEAD
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     if (event.target.innerWidth < 768) {
@@ -31,6 +21,4 @@ export class Feed1000Component implements OnInit {
       this.renderer.setStyle(this.myDiv.nativeElement, 'display', 'none');
     }
   }
-=======
->>>>>>> dfe13eb999eacf4df21f0d47caadae146b72c598
 }
